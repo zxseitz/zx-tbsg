1ff5d37997863301baa6e190b61eb61680e3a4fb
Claudio <zxseitz@gmail.com>
2020-11-05 10:37:04 +0100

    initial commit

---
+++ src/main/java/ch/zxseitz/tbsg/TbsgApplication.java
@@ -0,0 +1,13 @@
+package ch.zxseitz.tbsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbsgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbsgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zxseitz/tbsg/TbsgApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zxseitz.tbsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
