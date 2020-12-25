29d17d2f97399bafc528c85c82aa264f7a43e9c9
Claudio <zxseitz@gmail.com>
2020-12-25 18:27:58 +0100

    implemented new simple websocket connection

--- server/src/main/java/ch/zxseitz/tbsg/server/websocket/SocketHandler.java
+++ server/src/main/java/ch/zxseitz/tbsg/server/websocket/SocketHandler.java
@@ -1,9 +1,17 @@
-package ch.zxseitz.tbsg.server.api;
+package ch.zxseitz.tbsg.server.websocket;
 
+import ch.zxseitz.tbsg.server.security.JwtUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -12,10 +20,12 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
-public class MessageController extends TextWebSocketHandler {
+public class SocketHandler extends TextWebSocketHandler {
+    private static final Logger logger = LoggerFactory.getLogger(SocketHandler.class);
+
     private final List<WebSocketSession> sessions;
 
-    public MessageController() {
+    public SocketHandler() {
         this.sessions = new CopyOnWriteArrayList<>();
     }
 
@@ -34,6 +44,13 @@ public class MessageController extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         //the messages will be broadcasted to all users.
+        logger.info("New websocket client: {}", session.getId());
         sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        logger.info("Websocket client disconnected: {}", session.getId());
+        sessions.remove(session);
     }
 }
--- server/src/main/java/ch/zxseitz/tbsg/server/websocket/WebSocketConfig.java
+++ server/src/main/java/ch/zxseitz/tbsg/server/websocket/WebSocketConfig.java
@@ -1,2 +1,41 @@
-package ch.zxseitz.tbsg.server.websocket;public class WebSocketConfig {
+package ch.zxseitz.tbsg.server.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.Map;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket")
+                .setAllowedOrigins("*")  // handles unexpected response code 403
+                .addInterceptors(
+                        new HttpSessionHandshakeInterceptor() {
+                            @Override
+                            public void afterHandshake(ServerHttpRequest request,
+                                                       ServerHttpResponse response, WebSocketHandler wsHandler,
+                                                       @Nullable Exception ex) {
+                                super.afterHandshake(request, response, wsHandler, ex);
+                            }
+
+                            @Override
+                            public boolean beforeHandshake(ServerHttpRequest request,
+                                                           ServerHttpResponse response, WebSocketHandler wsHandler,
+                                                           Map<String, Object> attributes) throws Exception {
+                                //todo auth
+                                return true;
+                            }
+                        }
+                );
+    }
 }
