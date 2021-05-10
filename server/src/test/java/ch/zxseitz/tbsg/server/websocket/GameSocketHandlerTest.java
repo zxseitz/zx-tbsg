76e8db66716ede48033dc62960ab078c4697d03a
Claudio <zxseitz@gmail.com>
2021-05-10 11:32:34 +0200

    Impl SocketHandlerTest test challenge

--- server/src/test/java/ch/zxseitz/tbsg/server/websocket/GameSocketHandlerTest.java
+++ server/src/test/java/ch/zxseitz/tbsg/server/websocket/GameSocketHandlerTest.java
@@ -1,22 +1,32 @@
 package ch.zxseitz.tbsg.server.websocket;
 
 import ch.zxseitz.tbsg.server.games.GameProxy;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GameSocketHandler.class, ConcurrentHashMap.class, Client.class})
+@PrepareForTest({GameSocketHandler.class, ConcurrentHashMap.class, Client.class,
+        MessageManager.class, TextMessage.class})
 public class GameSocketHandlerTest {
     private final GameProxy proxy;
     private final ConcurrentHashMap<String, Client> lobby;
@@ -26,6 +36,7 @@ public class GameSocketHandlerTest {
         this.proxy = mock(GameProxy.class);
         this.lobby = mock(ConcurrentHashMap.class);
         whenNew(ConcurrentHashMap.class).withNoArguments().thenReturn(this.lobby);
+        mockStatic(MessageManager.class);
     }
 
     @Before
@@ -36,20 +47,23 @@ public class GameSocketHandlerTest {
     @Test
     public void testConnect() {
         String clientId = "clientId";
+        String clientMessage = "client message";
         try {
             var session = mock(WebSocketSession.class);
-            doReturn(clientId).when(session).getId();
             var client = mock(Client.class);
-            whenNew(Client.class).withArguments(session).thenReturn(client);
+            doReturn(clientId).when(session).getId();
             doReturn(clientId).when(client).getId();
+            whenNew(Client.class).withArguments(session).thenReturn(client);
+            when(MessageManager.createIdMessage(client)).thenReturn(clientMessage);
+
             var handler = new GameSocketHandler(proxy);
             handler.afterConnectionEstablished(session);
 
             verify(lobby, times(1)).put(clientId, client);
-            verify(client, times(1)).send(anyString());
+            verify(client, times(1)).send(clientMessage);
         } catch (Exception e) {
             e.printStackTrace();
-            Assert.fail();
+            fail();
         }
     }
 
@@ -69,9 +83,73 @@ public class GameSocketHandlerTest {
             verify(lobby, times(1)).remove(clientId);
         } catch (Exception e) {
             e.printStackTrace();
-            Assert.fail();
+            fail();
         }
     }
 
     // TODO: message handler
+
+    @Test
+    public void testMessageChallenge() {
+        int eventCode = MessageManager.CLIENT_CHALLENGE;
+        String clientId = "clientId";
+        String opponentId = "opponentId";
+        String payload = "payload";
+        String opponentChallengeMessage = "message";
+        try {
+            var message = PowerMockito.mock(TextMessage.class);  // final class
+            var args = mock(JsonNode.class);
+            var client = mock(Client.class);
+            var opponent = mock(Client.class);
+            var session = mock(WebSocketSession.class);
+            var challenges = new HashSet<Client>(1);
+            doReturn(clientId).when(session).getId();
+            doReturn(client).when(lobby).get(clientId);
+            doReturn(opponent).when(lobby).get(opponentId);
+            doReturn(challenges).when(client).getChallenges();
+            doReturn(payload).when(message).getPayload();
+            when(MessageManager.parseClientMessage(payload)).thenReturn(Map.entry(eventCode, args));
+            when(MessageManager.readClientArgument(args, "opponent", String.class)).thenReturn(opponentId);
+            when(MessageManager.createChallengeMessage(client)).thenReturn(opponentChallengeMessage);
+
+            var handler = new GameSocketHandler(proxy);
+            handler.handleTextMessage(session, message);
+
+            assertTrue(challenges.contains(opponent));
+            verify(opponent).send(opponentChallengeMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testMessageChallengeNoOpponent() {
+        int eventCode = MessageManager.CLIENT_CHALLENGE;
+        String clientId = "clientId";
+        String opponentId = "opponentId";
+        String payload = "payload";
+        String errorMessage = "error";
+        try {
+            var message = PowerMockito.mock(TextMessage.class);  // final class
+            var args = mock(JsonNode.class);
+            var client = mock(Client.class);
+            var session = mock(WebSocketSession.class);
+            doReturn(clientId).when(session).getId();
+            doReturn(client).when(lobby).get(clientId);
+            doReturn(null).when(lobby).get(opponentId);
+            doReturn(payload).when(message).getPayload();
+            when(MessageManager.parseClientMessage(payload)).thenReturn(Map.entry(eventCode, args));
+            when(MessageManager.readClientArgument(args, "opponent", String.class)).thenReturn(opponentId);
+            when(MessageManager.createErrorMessage(anyString())).thenReturn(errorMessage);
+
+            var handler = new GameSocketHandler(proxy);
+            handler.handleTextMessage(session, message);
+
+            verify(client).send(errorMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
 }
